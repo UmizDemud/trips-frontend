8baa3f1b645dddf1172b636decc13409b2061b41
UmizDemud <pikagchu0@gmail.com>
2025-04-01 09:19:04 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,10 @@ export default async function Home() {
 
   // This fetch is here to facilitate Render service to startup the backend. (Cold start)
 
-  const data: Trip[] = await fetch(process.env.NEXT_PUBLIC_API_URL! + "/api/trip")
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
+  let data: void | Response | null = await fetch(process.env.NEXT_PUBLIC_API_URL! + "/api/trip")
     .catch(console.error)
 
+  data = null;
 
   return (
     <div>
